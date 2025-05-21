b5f31861f044f8c0895360de420516efebd0e5de
katherine-zhou-ics4u1-02-25 <katherine.zhou3@student.tdsb.on.ca>
2025-05-21 17:28:56 +0000

    Update Child

--- src/main/java/Child.java
+++ src/main/java/Child.java
@@ -1,3 +1,53 @@
-class Child extends Person{
+import java.util.ArrayList;
 
+public class Child extends Person{
+    private Parent parent1;
+    private Parent parent2;
+    private ArrayList<Child> siblings;
+
+
+    public Child(String name, int age, Parent parent1, Parent parent2){
+        super(name, age);
+        this.parent1 = parent1;
+        this.parent2 = parent2;
+        this.siblings = new ArrayList<>();
+    }
+
+
+    public String getName(){
+        return super.getName();
+    }
+
+    public void setName(String name){
+        super.setName(name);
+    }
+
+    public int getAge() {
+        return super.getAge();
+    }
+
+    public void setAge(int age){
+        super.setAge(age);
+    }
+
+    public Parent getParent1(){
+        return this.parent1;
+    }
+
+    public Parent getParent2(){
+        return this.parent2;
+
+    }
+
+    public ArrayList<Child> getSiblings(){
+        return this.siblings;
+    }
+
+    public void setSiblings(ArrayList<Child> siblings){
+        this.siblings = siblings;
+    }
+
+    public void addSibling(Child sibling){
+        this.siblings.add(sibling);
+    }
 }
--- src/main/java/Parent.java
+++ src/main/java/Parent.java
@@ -1,3 +1,38 @@
+import java.util.ArrayList;
+
 class Parent extends Person{
-    
+    private Parent spouse;
+    private ArrayList<Child> children;
+
+    public Parent (String name, int age, Parent spouse){
+        super (name, age);
+        this.spouse = spouse;
+        this.children = new ArrayList<>();
+    }
+
+    public Parent (String name, int age){
+        super (name, age);
+        this.spouse = null;
+        this.children = new ArrayList<>();
+    }
+
+    public Parent getSpouse(){
+        return this.spouse;
+    }
+
+    public void setSpouse(Parent spouse){
+        this.spouse = spouse;
+    }
+
+    public ArrayList<Child> getChildren(){
+        return this.children;
+    }
+
+    public void setChildren (ArrayList<Child> children){
+        this.children = children;
+    }
+
+    public void addChild(Child child){
+        children.add(child);
+    }
 }
